da53bfc066ddca51ad8a4537d7247eddebfb0367
mirkotorrisi <mirko.torrisi92@gmail.com>
2021-02-07 22:50:53 +0100

    switch light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import "./App-light.css";
-
+import { useState, useEffect } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import { LogInComponent } from "./screens/LogInComponent";
 import { RegisterComponent } from "./screens/RegisterComponent";
@@ -12,10 +11,37 @@ import { NavBar } from "./components/NavBar";
 import { Footer } from "./components/Footer";
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  const handleButtonClick = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  useEffect(() => {
+    var head = document.head;
+    var link = document.createElement("link");
+
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = `./App-${darkTheme ? "dark" : "light"}.css`;
+
+    head.appendChild(link);
+
+    return () => {
+      head.removeChild(link);
+    };
+  }, [darkTheme]);
   return (
     <div>
       <NavBar />
       <div className="App">
+        <button
+          className="switch-theme"
+          title={`turn ${darkTheme ? "on" : "off"} lights!`}
+          onClick={() => handleButtonClick()}
+        >
+          <i class={`fas fa-${darkTheme ? "moon" : "sun"}`}></i>
+        </button>
         <Switch>
           <Route path="/" exact component={BetComponent} />
           <Route path="/login" component={LogInComponent} />
