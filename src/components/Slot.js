5dde30e4d8df56660aa5e08c5e4d2d14ffe5b6eb
mirkotorrisi <mirko.torrisi92@gmail.com>
2021-10-12 23:40:55 +0200

    changed images

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -1,15 +1,10 @@
 import { useEffect, useState, useRef } from "react";
 import * as slotresources from "../assets/slotresources";
-import {spinSlot} from '../service/index'
+import { spinSlot } from "../service/index";
 let prevTime = 0;
 export const Slot = () => {
-  const {
-    background,
-    assets,
-    carouselStyle,
-    numOfWheels,
-    spin,
-  } = slotresources.slotresources;
+  const { background, assets, carouselStyle, numOfWheels, spin } =
+    slotresources.slotresources;
   const [interacted, setInteracted] = useState(false);
   const [repeat, setRepeat] = useState(0);
   const [angle, setAngle] = useState(0);
@@ -18,13 +13,13 @@ export const Slot = () => {
       Math.floor(Math.random() * assets.length)
     )
   );
-  const [final, setFinal] = useState([])
+  const [final, setFinal] = useState([]);
   const [win, setwin] = useState(null);
   const [sum, setsum] = useState(0);
   const [tick, setTick] = useState(0);
 
   const tz = Math.round(
-    carouselStyle.height / 2 / Math.tan(Math.PI / assets.length) 
+    carouselStyle.height / 2 / Math.tan(Math.PI / assets.length)
   );
   const basicAngle = 360 / assets.length;
   useFrameLoop((time) => {
@@ -33,20 +28,25 @@ export const Slot = () => {
     setTick(time); // this is my frame rate
   });
   useEffect(() => {
-    if (JSON.stringify(wheels)==JSON.stringify(final)) return;
+    if (JSON.stringify(wheels) == JSON.stringify(final)) return;
     setWheels(
-      wheels.map((wheel, i) => 
-        wheel%assets.length !== final[i] ? wheel + 1 : wheel)
+      wheels.map((wheel, i) =>
+        wheel % assets.length !== final[i] ? wheel + 1 : wheel
+      )
     );
   }, [tick]);
   const handleSpin = () => {
-    setwin(null);  
-    setFinal([])
+    setwin(null);
+    setFinal([]);
     !interacted && setInteracted(true);
-    setTimeout(async ()=>{
-      const res = await spinSlot(2, numOfWheels, assets.length)
-      if(res){ setFinal(res.results); res.duplicates.length>0 && setwin(res.duplicates[0]); res.sum && setsum(res.sum)}
-    }, Math.random()*5000)
+    setTimeout(async () => {
+      const res = await spinSlot(2, numOfWheels, assets.length);
+      if (res) {
+        setFinal(res.results);
+        res.duplicates.length > 0 && setwin(res.duplicates[0]);
+        res.sum && setsum(res.sum);
+      }
+    }, Math.random() * 10000);
   };
 
   useEffect(() => {
@@ -65,82 +65,98 @@ export const Slot = () => {
   }, [interacted, repeat]);
 
   return (
-      <div
-        style={{
-          perspective: "500px",
-        display:"flex",
-        }}
-      >   
-            <button onClick={() => handleSpin()} style={spin} >{"SPIN"}</button>
-            { (win || win === 0) &&    
-            <h1 style={{  position: "absolute", 
-                          top: 327, left:201,
-                          background: "#f2f2f2", 
-                          border : "2px solid "+assets[win].color , 
-                          borderRadius: "10px",
-                          fontSize: 48, 
-                          color: assets[win].color, 
-                          zIndex:1000100}}>
-               {assets[win].src.split(".")[0]}
-            </h1>
-            }
-            { (win || win === 0) &&           
-            <h1 style={{  position: "absolute",
-                          top:-100,left:"20vw", 
-                          fontSize: 72,
-                          background: "#f2f2f2", 
-                          border : "2px solid "+assets[win].color, 
-                          borderRadius: "10px",
-                          color: assets[win].color, 
-                          zIndex:1000100}}>
-               {sum}$
-            </h1>}
-         <img
-      src={"img/" + background.src}
+    <div
       style={{
-        ...background.style,
+        perspective: "500px",
+        display: "flex",
       }}
-    />
-        {wheels.map((wheel, i) => {
-          return (
-            <div
-              style={{
-                position: "absolute",
-                left: carouselStyle.left + i*25  + "%",
+    >
+      <button onClick={() => handleSpin()} style={spin}>
+        {"SPIN"}
+      </button>
+      {(win || win === 0) && (
+        <h1
+          style={{
+            position: "absolute",
+            top: 327,
+            left: 201,
+            background: "#f2f2f2",
+            border: "2px solid " + assets[win].color,
+            borderRadius: "10px",
+            fontSize: 48,
+            color: assets[win].color,
+            zIndex: 1000100,
+          }}
+        >
+          {assets[win].src.split(".")[0]}
+        </h1>
+      )}
+      {(win || win === 0) && (
+        <h1
+          style={{
+            position: "absolute",
+            top: -100,
+            left: "20vw",
+            fontSize: 72,
+            background: "#f2f2f2",
+            border: "2px solid " + assets[win].color,
+            borderRadius: "10px",
+            color: assets[win].color,
+            zIndex: 1000100,
+          }}
+        >
+          {sum}$
+        </h1>
+      )}
+      <img
+        src={"img/" + background.src}
+        style={{
+          ...background.style,
+        }}
+      />
+      {wheels.map((wheel, i) => {
+        return (
+          <div
+            style={{
+              position: "absolute",
+              left: carouselStyle.left + i * 25 + "%",
 
-                transform: `translateZ(-${tz}px) rotateX(${
-                  !interacted ? angle : (360 / assets.length) * wheel * -1
-                }deg)`,
-                transformStyle: "preserve-3d",
-                height: carouselStyle.height,
+              transform: `translateZ(-${tz}px) rotateX(${
+                !interacted ? angle : (360 / assets.length) * wheel * -1
+              }deg)`,
+              transformStyle: "preserve-3d",
+              height: carouselStyle.height,
 
-                transition: "transform 1s",
-                top: background.style.height/3
-              }}
-            >
-               {assets &&
-                assets.map((img, wheel) => {
-                  return (
-                    <img
-                      src={"img/" + img.src}
-                      style={{
-                        transform: `rotateX(${
-                          basicAngle * wheel
-                        }deg) translateZ(${tz}px)`,
-                        background: win === wheel && Math.round(tick)%2===0 ? assets[wheel].color : "#f2f2f2",
-                        width: carouselStyle.width,
-                        maxWidth: 192,
-                        height: carouselStyle.height,
-                        position: "absolute",
-                        zIndex: 10001,
-                      }}
-                    />
-                  );
-                })}
-            </div>
-          );
-        })}
-      </div>
+              transition: "transform 1s",
+              top: background.style.height / 3,
+            }}
+          >
+            {assets &&
+              assets.map((img, wheel) => {
+                return (
+                  <img
+                    src={"img/" + img.src}
+                    style={{
+                      transform: `rotateX(${
+                        basicAngle * wheel
+                      }deg) translateZ(${tz}px)`,
+                      background:
+                        win === wheel && Math.round(tick) % 2 === 0
+                          ? assets[wheel].color
+                          : "#f2f2f2",
+                      width: carouselStyle.width,
+                      maxWidth: 192,
+                      height: carouselStyle.height,
+                      position: "absolute",
+                      zIndex: 10001,
+                    }}
+                  />
+                );
+              })}
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
